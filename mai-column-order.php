30ebe90d84bc89a81930b31ce3f39a89aae4e463
JiveDig <jivetheuniversal@gmail.com>
2025-01-27 11:03:47 -0500

    cleanup test code

--- mai-column-order.php
+++ mai-column-order.php
@@ -9,14 +9,13 @@
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       mai-column-order
- *
- * @package CreateBlock
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+add_action( 'enqueue_block_editor_assets', 'mai_column_order_editor_assets' );
 /**
  * Enqueue editor assets.
  *
@@ -24,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @return void
  */
-add_action( 'enqueue_block_editor_assets', function() {
+function mai_column_order_editor_assets() {
 	// Enqueue the editor JavaScript
 	wp_enqueue_script(
 		'mai-column-order',
@@ -33,22 +32,11 @@ add_action( 'enqueue_block_editor_assets', function() {
 		filemtime( plugin_dir_path( __FILE__ ) . 'build/index.js' ),
 		true
 	);
-
-	// // Add editor-specific styles
-	// $editor_css = '
-	// 	@media (max-width: 781px) {
-	// 		.wp-block-column[data-order-mobile] {
-	// 			order: var(--order-mobile, 0);
-	// 		}
-	// 	}
-	// ';
-	// wp_add_inline_style( 'wp-edit-blocks', $editor_css );
-});
+}
 
 add_action( 'init', 'mai_column_order_block_styles' );
 /**
- * Enqueue block styles
- * (Applies to both frontend and Editor)
+ * Enqueue block styles (Applies to both frontend and Editor).
  *
  * @since 0.1.0
  *
@@ -65,48 +53,3 @@ function mai_column_order_block_styles() {
 		]
 	);
 }
-
-// /**
-//  * Enqueue frontend assets.
-//  *
-//  * @return void
-//  */
-// add_action( 'enqueue_block_assets', function() {
-// 	// Add frontend styles
-// 	$frontend_css = '
-// 		@media (max-width: 781px) {
-// 			.wp-block-column[style*="--order-mobile"] {
-// 				order: var(--order-mobile, 0);
-// 			}
-// 		}
-// 	';
-// 	wp_add_inline_style( 'wp-block-column', $frontend_css );
-// });
-
-// add_action( 'init', function() {
-// 	wp_enqueue_block_style(
-// 		'core/column',
-// 		[
-// 			'handle' => 'mai-column-order',
-// 			'src'    => plugins_url( 'build/style.css', __FILE__ ),
-// 			'path'   => plugin_dir_path( __FILE__ ) . 'build/style.css',
-// 		]
-// 	);
-// });
-
-
-// add_action( 'enqueue_block_assets', 'add_columns_block_inline_style' );
-// function add_columns_block_inline_style() {
-// 	// Enqueue the existing stylesheet for the columns block (core block styles).
-// 	wp_enqueue_style( 'wp-block-library' );
-
-// 	// Add your inline CSS.
-// 	$custom_css = '
-// 		@media (max-width: 781px) {
-// 			.wp-block-column[style*="--order-mobile"] {
-// 				order: var(--order-mobile, 0);
-// 			}
-// 		}
-// 	';
-// 	wp_add_inline_style( 'wp-block-library', $custom_css );
-// }
