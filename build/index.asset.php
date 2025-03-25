dfd80e9b3b5940967d957f10f8f8c16d7de146bf
JiveDig <jivetheuniversal@gmail.com>
2025-03-25 16:25:54 -0400

    npm run build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n'), 'version' => '3eee211498050e097c7a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n'), 'version' => '7cc657dec6de43ef5c7e');
