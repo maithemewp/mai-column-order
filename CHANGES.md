0576d511487a9d810af56641bf1800f44e886133
JiveDig <540990+JiveDig@users.noreply.github.com>
2026-05-26 16:03:09 -0400

    0.1.2 update the updater for PHP 8.4 support and switch to tag-based updates

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,9 @@
 ## Changelog
 
+### 0.1.2 (2026-05-26)
+* Changed: Updated the updater for PHP 8.4 support.
+* Changed: Switched to tag-based updates.
+
 ### 0.1.1 (2025-01-29)
 * Added: Automatic updates via GitHub.
 
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,7 +7,7 @@ namespace Composer\Autoload;
 class ComposerStaticInit498cd9734dd600df8e6d83e79142eacb
 {
     public static $files = array (
-        '256558b1ddf2fa4366ea7d7602798dd1' => __DIR__ . '/..' . '/yahnis-elsts/plugin-update-checker/load-v5p5.php',
+        'bc0af1337b39f0d750e835f5263eb646' => __DIR__ . '/..' . '/yahnis-elsts/plugin-update-checker/load-v5p7.php',
     );
 
     public static $classMap = array (
